c8e488d0985bd7863609f0cdf9c104580e5e76a7
odylomv <odylomv@gmail.com>
2021-06-20 19:29:10 +0300

    added a rule to enforce keyword-weight on all keywords

--- main.pl
+++ main.pl
@@ -9,4 +9,20 @@ generate_keyword_score_pairs([Keyword|ListOfKeywords]) :-
     parse(Keyword),
     generate_keyword_score_pairs(ListOfKeywords).
 
-parse(Keyword) :- print(Keyword), nl.
+% Keyword matches the pattern keyword-weight
+ensure_full_keyword(Keyword, FullKeyword) :- 
+    pairs_keys_values([Keyword], [_], [Weight]),
+    number(Weight),                                     % Required for words with dashes (eg. semi-transparent)
+    FullKeyword = Keyword,
+    !.                                                  % Prevent unnecessary second pass
+
+ensure_full_keyword(Keyword, FullKeyword) :- 
+    % Keyword lacks weight
+    \+ (
+        pairs_keys_values([Keyword], [_], [Weight]),
+        number(Weight)                                  % Required for words with dashes (eg. semi-transparent)
+    ),                   
+    pairs_keys_values([FullKeyword], [Keyword], [1]).   % Add default weight (1)
+
+
+parse(Keyword) :- ensure_full_keyword(Keyword, FullKeyword), print(FullKeyword), nl.

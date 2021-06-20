32356549147e0fd0ff3db78fda56f900fe7ce13f
odylomv <odylomv@gmail.com>
2021-06-20 17:39:31 +0300

    Literally no significant progress but u gotta see progress to make progress so MAKE PROGRESS

--- main.pl
+++ main.pl
@@ -1,3 +1,12 @@
 % Odysseas Lomvardeas 3362
 % Alexandros Sofoulakis 3346
 % Melina Zikou 3357
+
+query(ListOfKeywords) :- generate_keyword_score_pairs(ListOfKeywords).
+
+generate_keyword_score_pairs([]).
+generate_keyword_score_pairs([Keyword|ListOfKeywords]) :- 
+    parse(Keyword),
+    generate_keyword_score_pairs(ListOfKeywords).
+
+parse(Keyword) :- print(Keyword), nl.

9c6d3f99ff87e354bf02043279e57be5cc1e3119
melinaz@live.com <melinaz@live.com>
2021-06-21 20:49:07 +0300

    Comments and variable names in calculate section

--- main.pl
+++ main.pl
@@ -80,6 +80,7 @@ parse(Keyword, [FullKeyword|SubKeywords]) :-
 
 
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%% SECTION: SORT RESULTS %%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -111,80 +112,58 @@ print_results([HeadTitles|TailTitles], [HeadScores|TailScores]):-
 %%%%%%%%%%%%%%% SECTION: SCORE CALCULATION %%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% parameters: input - title or subject of session
-%             keywordPairs - keyword pairs(word - values)
-% 
-% return: points of keyword if kewyord is found in session
-%         0 
-
-is_in_session(Input, FullKeyword, Score) :-
-	pairs_keys_values([FullKeyword], [Keyword], [Score]),
-	sub_string(case_insensitive, Keyword, Input),
-	% print(Score), nl,
+% Check if a keyword exists in a title or subject of a session
+% If it exists - return the score of the keyword
+	is_in_session(Input, FullKeyword, Score) :-
+	pairs_keys_values([FullKeyword], [Keyword], [Score]),			% Seperate keyword and score          
+	sub_string(case_insensitive, Keyword, Input),					% Check if keyword is in title or subject
 	!.
 
+% If it doesn't exist- return 0
 is_in_session(Input, FullKeyword, 0) :-
-	pairs_keys([FullKeyword], [Keyword]),
-	% print(Keyword),
-	\+ sub_string(case_insensitive, Keyword, Input),
-	% print(0), nl,
+	pairs_keys([FullKeyword], [Keyword]),							% Seperate keyword and score		
+	\+ sub_string(case_insensitive, Keyword, Input),				% Check if keyword is not in title or subject
 	!.
 
-% parameters: title - the title of a session
-%             list  - keyword pairs(word - values)
-% 
-% return: score associated with the title  
+
+% Calculate the score associated with the title of a session
 title_score(_, [], 0).
-title_score(Title, [Head|Tail], Score):-
-	title_score(Title, Tail, RemainingScore),
-	is_in_session(Title, Head, TitleScore),
-	Score is TitleScore * 2 + RemainingScore.
+title_score(Title, [KeywordPair|RemainingKeywordPairs], Score):-
+	title_score(Title, RemainingKeywordPairs, RemainingScore),		
+	is_in_session(Title, KeywordPair, TitleScore),					% Check if keyword is in title
+	Score is TitleScore * 2 + RemainingScore.						% Multiply the score of title * 2 (if the keyword is in the title)
 
 
-% parameters: subject - the subject of a session
-%             list  - keyword pairs(word - values)
-% 
-% return: list with subject scores of a session
+% Calculate the score associated with the subject of a session
 subject_score(_, [], 0).
-subject_score(Subject, [Head|Tail], Score):-
-	subject_score(Subject, Tail, RemainingScore),
-	is_in_session(Subject, Head, SubjectScore),
-	Score is SubjectScore + RemainingScore.
+subject_score(Subject, [KeywordPair|RemainingKeywordPairs], Score):-
+	subject_score(Subject, RemainingKeywordPairs, RemainingScore),
+	is_in_session(Subject, KeywordPair, SubjectScore),				% Check if keyword is in subject
+	Score is SubjectScore + RemainingScore.							% Add the score of subject (if the keyword is in the subject)
 
 
-% parameters: list 1 - session subjects
-%             list - keyword pairs(word - values)
-% 
-% return: list of scores associated with the subject  
+% Calculate the scores associated with the subjects of a session and store them in a list
 subject_total_score([], _, []).
-subject_total_score([Head|Tail], KeywordPairs, Score):-
-	subject_total_score(Tail, KeywordPairs, RemainingScore),
-	subject_score(Head, KeywordPairs, SubjectScore),
-	append([SubjectScore], RemainingScore, Score).
+subject_total_score([Subject|RemainingSubjects], KeywordPairs, Score):-
+	subject_total_score(RemainingSubjects, KeywordPairs, RemainingScore),
+	subject_score(Subject, KeywordPairs, SubjectScore),
+	append([SubjectScore], RemainingScore, Score).					% Add to the list of subject scores the subject score
 
 
-% parameters: Title - session title
-%             Subjects - list of session subjects
-%             list  - keyword pairs(word - values)
-% 
-% return: total score of session which is 1000 * Max + Sum
+% Calculate the total score of session which is 1000 * Max + Sum
 session_score(Title, Subjects, KeywordPairs, TotalScore):-
 	title_score(Title, KeywordPairs, TitleScore),
 	subject_total_score(Subjects, KeywordPairs, SubjectScores),
-	append([TitleScore], SubjectScores, Scores),
-	sum_list(Scores, Sum),
-	max_list(Scores, Max),
-	TotalScore is 1000 * Max + Sum.
+	append([TitleScore], SubjectScores, Scores),					% Add to the list of title scores the title score
+	sum_list(Scores, Sum),											% Sum the list of scores
+	max_list(Scores, Max),											% Find the max element of the list of scores
+	TotalScore is 1000 * Max + Sum.									% Calculate the session score
 
 
-% parameters: list 1 - list of session titles
-%             list 2 - list of session subjects
-%             list  - keyword pairs(word - values)
-% 
-% return: list with total scores of all sessions
+% Calculate the total score of all sessions and store them in a list
 score([], [], _, []).
-score([HeadTitle|TailTitle], [HeadSubject|TailSubject], KeywordPairs, TotalScores):-
-	score(TailTitle, TailSubject, KeywordPairs, RemainingScores),
-	session_score(HeadTitle, HeadSubject, KeywordPairs, SessionScore),
+score([Title|RemainingTitles], [SessionSubjects|RemainingSessionSubjects], KeywordPairs, TotalScores):-
+	score(RemainingTitles, RemainingSessionSubjects, KeywordPairs, RemainingScores),
+	session_score(Title, SessionSubjects, KeywordPairs, SessionScore),
 	append([SessionScore], RemainingScores, TotalScores),
 	!.

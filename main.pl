3514e2b272a9724613247cd5db2a25b705985d30
odylomv <odylomv@gmail.com>
2021-06-21 22:03:51 +0300

    small optimizations and comments

--- main.pl
+++ main.pl
@@ -12,6 +12,7 @@
 % 3) 'glass' with weight 2
 
 
+% The entry rule
 query(ListOfKeywords) :- 
     generate_keyword_score_pairs(ListOfKeywords, ProcessedKeywords),
 
@@ -89,17 +90,15 @@ parse(Keyword, [FullKeyword|SubKeywords]) :-
 
 % Check if a keyword exists in a title or subject of a session
 % If it exists - return the score of the keyword
-is_in_session(Input, FullKeyword, Score) :-
-	pairs_keys_values([FullKeyword], [Keyword], [Score]),			% Seperate keyword and score          
-	sub_string(case_insensitive, Keyword, Input),					% Check if keyword is in title or subject
-	!.
-
+is_in_session(Input, KeywordPair, Score) :-
+	pairs_keys_values([KeywordPair], [Keyword], [Score]),			% Seperate keyword and score          
+	sub_string(case_insensitive, Keyword, Input).					% Check if keyword is in title or subject
+	
 % If it doesn't exist- return 0
-is_in_session(Input, FullKeyword, 0) :-
-	pairs_keys([FullKeyword], [Keyword]),							% Seperate keyword and score		
+is_in_session(Input, KeywordPair, 0) :-
+	pairs_keys([KeywordPair], [Keyword]),							% Seperate keyword and score		
 	\+ sub_string(case_insensitive, Keyword, Input),				% Check if keyword is not in title or subject
-	!.
-
+	!.																% Prevent unnecessary second pass
 
 % Calculate the score associated with the title of a session
 title_score(_, [], 0).
@@ -108,7 +107,6 @@ title_score(Title, [KeywordPair|RemainingKeywordPairs], Score):-
 	is_in_session(Title, KeywordPair, TitleScore),					% Check if keyword is in title
 	Score is TitleScore * 2 + RemainingScore.						% Multiply the score of title * 2 (if the keyword is in the title)
 
-
 % Calculate the score associated with the subject of a session
 subject_score(_, [], 0).
 subject_score(Subject, [KeywordPair|RemainingKeywordPairs], Score):-
@@ -116,14 +114,12 @@ subject_score(Subject, [KeywordPair|RemainingKeywordPairs], Score):-
 	is_in_session(Subject, KeywordPair, SubjectScore),				% Check if keyword is in subject
 	Score is SubjectScore + RemainingScore.							% Add the score of subject (if the keyword is in the subject)
 
-
 % Calculate the scores associated with the subjects of a session and store them in a list
 subject_total_score([], _, []).
 subject_total_score([Subject|RemainingSubjects], KeywordPairs, Score):-
-	subject_total_score(RemainingSubjects, KeywordPairs, RemainingScore),
+	subject_total_score(RemainingSubjects, KeywordPairs, TempScore),
 	subject_score(Subject, KeywordPairs, SubjectScore),
-	append([SubjectScore], RemainingScore, Score).					% Add to the list of subject scores the subject score
-
+	append([SubjectScore], TempScore, Score).					% Add to the list of subject scores the subject score
 
 % Calculate the total score of session which is 1000 * Max + Sum
 session_score(Title, Subjects, KeywordPairs, TotalScore):-
@@ -134,13 +130,12 @@ session_score(Title, Subjects, KeywordPairs, TotalScore):-
 	max_list(Scores, Max),											% Find the max element of the list of scores
 	TotalScore is 1000 * Max + Sum.									% Calculate the session score
 
-
 % Calculate the total score of all sessions and store them in a list
 score([], [], _, []).
 score([Title|RemainingTitles], [SessionSubjects|RemainingSessionSubjects], KeywordPairs, TotalScores):-
-	score(RemainingTitles, RemainingSessionSubjects, KeywordPairs, RemainingScores),
+	score(RemainingTitles, RemainingSessionSubjects, KeywordPairs, TempScores),
 	session_score(Title, SessionSubjects, KeywordPairs, SessionScore),
-	append([SessionScore], RemainingScores, TotalScores),			% Add to the list of session scores the session score
+	append([SessionScore], TempScores, TotalScores),				% Add to the list of session scores the session score
 	!.																% Prevent unnecessary second pass
 
 
@@ -150,14 +145,14 @@ score([Title|RemainingTitles], [SessionSubjects|RemainingSessionSubjects], Keywo
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Get a list of key-value pairs (title-score) and sort in descending order by score
-sort(TitlesScoresInitial, TitlesFinal, ScoresFinal) :-
-	transpose_pairs(TitlesScoresInitial, TempList),					% Flip the key-value pairs into value-key pairs and sort by score in ascending order
-	reverse(TempList, SortedList),									% Reverse to sort in descending order
+sort(TitleScorePairs, SortedTitles, SortedScores) :-
+	transpose_pairs(TitleScorePairs, TempList),						% Flip the key-value pairs into value-key pairs and sort by score in ascending order
+	reverse(TempList, SortedPairs),									% Reverse to sort in descending order
 	
-	pairs_values(SortedList, TitlesFinal),
-	pairs_keys(SortedList, ScoresFinal). 							% Separate lists and return them
+	pairs_keys_values(SortedPairs, SortedScores, SortedTitles).		% Separate lists and return them
 
 
+% Prints the results in the requested format
 print_results([], []).
 print_results([Title|RemainingTitles], [Score|RemainingScores]):-
 	write(' Session: '),
